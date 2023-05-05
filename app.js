a47b77785fd7bfb19fa8502502b4aa1ce6bdc56d
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-05-05 19:29:19 +0200

    Add files for frontend library

---
+++ app.js
@@ -0,0 +1,73 @@
+import express from 'express'
+import http from 'http'
+import nunjucks from 'nunjucks'
+import sassMiddleware from 'node-sass-middleware'
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+
+class Frontend {
+    _inbox = []
+    _outbox = []
+    server
+
+    constructor() {
+        const app = express()
+
+        nunjucks.configure('views', {
+            autoescape: true,
+            express: app
+        })
+
+        app.use(sassMiddleware({
+            src: path.join(__dirname, 'bootstrap'),
+            dest: path.join(__dirname, 'public'),
+            indentedSyntax: true, // true = .sass and false = .scss
+            sourceMap: true
+        }))
+
+        app.use(express.static(path.join(__dirname, 'public')))
+
+        app.get('/', (req, res, next) => {
+            let data = {
+                content: 'Hello world!',
+                title: 'Bootstrap example',
+                inbox: this.inbox,
+                outbox: this.outbox
+            }
+
+            res.render('index.njk', data)
+        })
+
+        app.get('/notifications/received', (req, res, next) => {
+            res.json(this._inbox)
+        })
+
+        app.get('/notifications/sent', (req, res, next) => {
+            res.json(this._outbox)
+        })
+
+        this.server = http.createServer(app)
+    }
+
+    start() {
+        this.server.listen('3000', () => {
+            console.log('Listening on port 3000')
+        })
+    }
+
+    get outbox() {
+        return this._outbox
+    }
+
+    get inbox() {
+        return this._outbox
+    }
+
+}
+
+new Frontend().start()
