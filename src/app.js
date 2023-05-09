83b051d1d3102e962678f7c3c18d1f8d2a120034
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-05-09 15:26:02 +0200

    Make server shutdown gracefullt

--- src/app.js
+++ src/app.js
@@ -57,11 +57,42 @@ export default class Frontend {
     }
 
     start() {
-        this.server.listen('3000', () => {
+        const server = this.server.listen('3000', () => {
             console.log('Listening on port 3000')
         })
-    }
 
+        // Graceful shutdown   
+        setInterval(() => server.getConnections(
+            (err, connections) => console.log(`${connections} connections currently open`)
+        ), 1000);
+        
+        process.on('SIGTERM', shutDown);
+        process.on('SIGINT', shutDown);
+        
+        let connections = [];
+        
+        server.on('connection', connection => {
+            connections.push(connection);
+            connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+        });
+        
+        function shutDown() {
+            console.log('Received kill signal, shutting down gracefully');
+            server.close(() => {
+                console.log('Closed out remaining connections');
+                process.exit(0);
+            });
+        
+            setTimeout(() => {
+                console.error('Could not close connections in time, forcefully shutting down');
+                process.exit(1);
+            }, 10000);
+        
+            connections.forEach(curr => curr.end());
+            setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+        }
+    }
+    
     get outbox() {
         return this._outbox
     }
